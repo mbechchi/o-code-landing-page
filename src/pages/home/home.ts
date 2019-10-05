cdd81e2247a2c372b686aaad0413f7aa7c745d1a
jguichot <jguichot@gmail.com>
2019-10-05 19:03:13 +0200

    [home] slider system ok

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,24 +11,40 @@ interface Member {
 // Init
 window.onload = () => {
 
+    let memberIndexSelected: number = 0;
+
+    // Member preview elements
     const memberWrapper = document.querySelector('#page-home #team #member-preview-wrapper');
     const memberImageEl = memberWrapper.querySelector('.member-image');
     const memberNameEl = memberWrapper.querySelector('.member-name');
     const memberStatusEl = memberWrapper.querySelector('.member-status');
     const memberXpEl = memberWrapper.querySelector('.member-xp');
     const memberInformationsEl = memberWrapper.querySelector('.member-informations');
+    const memberBtnNextEl = memberWrapper.querySelector('.btn-next');
+    const memberBtnPrevEl = memberWrapper.querySelector('.btn-prev');
 
+    // Members data and elements
     const members: Array<Member> = i18n.TEAM.MEMBERS;
     const membersEl = document.querySelectorAll('#page-home #team #members .member');
 
-    let memberIndexSelected: number = 0;
-
+    // Listen member click
     membersEl.forEach((el, index) => {
-        el.addEventListener('click', () => {
-            selectMember(index);
-        });
+        el.addEventListener('click', () => { selectMember(index); });
     });
 
+    // Listen next member button click
+    memberBtnNextEl.addEventListener('click', () => {
+        const nextIndex = (members.length === memberIndexSelected + 1) ? 0 : memberIndexSelected + 1;
+        selectMember(nextIndex);
+    });
+
+    // Listen previous member button click
+    memberBtnPrevEl.addEventListener('click', () => {
+        const nextIndex = (memberIndexSelected === 0) ? members.length - 1 : memberIndexSelected - 1;
+        selectMember(nextIndex);
+    });
+
+    // On member selected
     const selectMember = (memberIndex: number) => {
 
         const selectedMember: Member = members[memberIndex];
@@ -40,7 +56,7 @@ window.onload = () => {
         membersEl[memberIndex].classList.add('selected');
 
         // Set member preview
-        memberImageEl['style'].backgroundImage = `assets/images/${selectedMember.IMAGE}`;
+        memberImageEl['style'].backgroundImage = `url('assets/images/${selectedMember.IMAGE}')`;
         memberNameEl.innerHTML = selectedMember.NAME;
         memberStatusEl.innerHTML = selectedMember.STATUS;
         memberXpEl.innerHTML = selectedMember.XP;
