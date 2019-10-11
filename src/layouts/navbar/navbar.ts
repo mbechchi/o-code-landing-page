47f54e2cd0405eed52dc8b0ebfcae28ac2008a6c
jguichot <jguichot@gmail.com>
2019-10-11 13:55:48 +0200

    add navbar menu active animations

--- src/layouts/navbar/navbar.ts
+++ src/layouts/navbar/navbar.ts
@@ -2,12 +2,16 @@ const NAVBAR_ANIM_Y = 150;
 
 export class NavbarAnim {
 
+    private activatedMenuIndex: number = -1;
     private didScroll = false;
-    private navEl;
+    private navEl: HTMLElement;
+    private navMenuEl: NodeListOf<HTMLElement>;
+    private sections: Array<{scrollY: number, hash: string}> = [];
 
     constructor() {
 
         this.navEl = document.querySelector('nav');
+        this.navMenuEl = this.navEl.querySelectorAll('.col-menu ul li');
 
         // Init navbar
         this.scrollPage();
@@ -18,13 +22,54 @@ export class NavbarAnim {
         // Listen mobile menu button click
         const mobileMenuBtnEl = document.querySelector('#mobile-menu-btn');
         //mobileMenuBtnEl.addEventListener('click', this.onMobileMenuButtonClick.bind(this));
+
+        // Get all navbar-hash sections
+        const sectionsEl = document.querySelectorAll('section[navbar-hash]');
+        sectionsEl.forEach((el: HTMLElement) => {
+
+            const hash = el.getAttribute('id');
+            const offsetTop = el.offsetTop;
+
+            this.sections.push({
+                hash: hash,
+                scrollY: offsetTop
+            });
+        });
     }
 
     onPageScroll() {
 
+        this.setActiveMenu();
+
         if(!this.didScroll) {
             this.didScroll = true;
             setTimeout(this.scrollPage.bind(this), 250);
+        }
+    }
+
+    setActiveMenu() {
+
+        const scrollY = this.scrollY();
+        let menuIndex = -1;
+
+        this.sections.forEach((section, index) => {
+            if (section.scrollY < scrollY + (window.innerHeight / 2)) {
+                menuIndex = index;
+                return;
+            }
+        });
+
+        if(this.activatedMenuIndex !== menuIndex) {
+
+            if(this.activatedMenuIndex > -1) {
+                this.navMenuEl[this.activatedMenuIndex].classList.remove('active');
+            }
+
+            if(menuIndex > -1) {
+                this.navMenuEl[menuIndex].classList.add('active');
+            }
+
+            this.activatedMenuIndex = menuIndex;
         }
     }
 
